6b7371d60bfca500f4301c6397fdee3a1ff33a59
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2026-02-16 00:18:21 +0530

    langgraph and structured output

--- server/worker.py
+++ server/worker.py
@@ -1,12 +1,45 @@
 from langchain.chat_models import init_chat_model
-from langchain_core.messages import HumanMessage, SystemMessage
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+import json
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph import StateGraph, MessagesState, START, END
 from pydantic import BaseModel, Field
-from typing import Literal
-
+from typing import Literal, List
+load_dotenv()
 class State(MessagesState):
     pass
 class Schema(BaseModel):
     type: Literal["html","css","js"] = Field(description="Type of the file")
-    code: str = Field("The code of the type")
+    code: str = Field(description="The code of ther type")
+class AIoutput(BaseModel):
+    output: List[Schema]
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+llm_withCodeStructure = llm.with_structured_output(schema=AIoutput)
+
+def webBuilder(state:State):
+    response = llm_withCodeStructure.invoke(state.get("messages"))
+    return {
+"messages": [
+            AIMessage(content=response.model_dump_json())
+        ]
+    }
+graph_builder = StateGraph(State)
+graph_builder.add_node("webBuilder", webBuilder)
+graph_builder.add_edge(START,"webBuilder")
+graph_builder.add_edge("webBuilder",END)
+graph = graph_builder.compile()
+systemPrompt = "You are an ai website builder agent which returns html,css,js files"
+def process(query:str):
+    initial_state = {
+        "messages":[SystemMessage(systemPrompt), HumanMessage(query)]
+    }
+    for chunk in graph.stream(initial_state, stream_mode="values"):
+        msg = chunk["messages"][-1]
+        if msg.type=="ai":
+            data = json.loads(msg.content)
+            return data
+while True:
+    query = input(">> ")
+    print(process(query=query))
